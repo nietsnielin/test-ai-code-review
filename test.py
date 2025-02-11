fe2af6987ae31ef7ac07c7c2a0da7a337a200c9f
JIMLIN <nietsnielin@gmail.com>
2025-02-11 17:05:35 +0800

    1.06

---
+++ test.py
@@ -0,0 +1,9 @@
+# 讀取文件
+with open('input.txt', 'r') as file:
+    content = file.read()
+    print("文件內容：", content)
+ 
+# 寫入文件
+with open('output.txt', 'w') as file:
+    file.write("這是寫入的新內容。")
+    print("內容已寫入output.txt")s123
